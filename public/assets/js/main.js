7d6a2fccc1e89031e8f084b7fa6bfdba14c6646c
MartaBelmonte <martabelmonts5@gmail.com>
2023-06-09 23:50:20 +0200

    show-list-part-one

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -1,31 +1,38 @@
 'use strict';
 
-const characterListContainer = document.querySelector(".js_character-list");
 
+// Paso 3: Obtener el array de personajes desde el API con fetch()
 fetch('api.disneyapi.dev/character?pageSize=50')
   .then(response => response.json())
   .then(data => {
-    const characters = data.data;
-
-    characters.forEach(character => {
-      const characterCard = document.createElement("div");
-      characterCard.classList.add("character-card");
-
-      const characterImage = document.createElement("img");
-      characterImage.src = character.imageUrl;
-      characterImage.alt = character.name;
-
-      const characterName = document.createElement("h2");
-      characterName.textContent = character.name;
-
-      characterCard.appendChild(characterImage);
-      characterCard.appendChild(characterName);
-
-      characterListContainer.appendChild(characterCard);
-    });
+  const characterData = data.results;
+  console.log(characterData);
   })
   .catch(error => {
     console.error('Error', error);
 });
 
+//1. Datos API copiados
+const characterData = [
+  { name: "Mickey Mouse", image: "mickey.jpg" },
+  { name: "Donald Duck", image: "donald.jpg" },
+  // ... más personajes
+];
+
+//1Generar HTML para un único personaje con bucle
+const charactersContainer = document.querySelector('.js_character-list');
+for (let i = 0; i < characterData.length; i++) {
+  const character = characterData[i];
+  
+  const characterCard = `
+    <div class="card">
+      <p>${character.name}</p>
+      <img src="${character.image}" alt="${character.name}">
+    </div>
+  `;
+  
+  // Agregar la tarjeta al contenedor en el HTML
+  charactersContainer.innerHTML += characterCard;
+
+};
 //# sourceMappingURL=main.js.map
