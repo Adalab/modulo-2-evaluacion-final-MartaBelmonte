ce58e14ab7fa9b90df1401e05d17285448aad757
MartaBelmonte <martabelmonts5@gmail.com>
2023-06-10 19:28:44 +0200

    fav

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -1,17 +1,17 @@
 'use strict';
 
 let listCharactersApi = [];
+let listCharacterFavourite = [];
 const ulElement = document.querySelector('.js_ul_list');
 const url = 'https://api.disneyapi.dev/character?pageSize=50';
 
-// Paso 3: Obtener el array de personajes desde el API con fetch()
+//Obtener el array de personajes desde el API con fetch()
 fetch(url)
   .then(response => response.json())
   .then(data => {
-  console.log(data); //respuesta del servidor
-  listCharactersApi = data.characters;
-  renderCharacterList (listCharactersApi);
-  ulElement.innerHTML =  renderCharacter(data.characters[0]);
+  listCharactersApi = data.data;
+  renderCharacterList(listCharactersApi);
+ 
   })
   .catch(error => {
     console.error('Error', error);
@@ -24,18 +24,17 @@ function renderCharacterList (listData) {
   }
 }
 
-function renderCharacter(characters) {
-
-  for (const character of characters) { 
+function renderCharacter(character) { 
     const html = `
       <div class="characters js_character-list">
       <p>Name: ${character.name}</p>
       <img src="${character.imageUrl}" alt="${character.name}" />
     </div>
     `;
-  }
 
   return html;
 }
+
+//Añadir a favoritos
 
 //# sourceMappingURL=main.js.map
