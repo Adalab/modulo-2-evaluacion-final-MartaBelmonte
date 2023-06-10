8d32ef3501f8767e80798ff71a2a4614cde3a209
MartaBelmonte <martabelmonts5@gmail.com>
2023-06-10 12:36:20 +0200

    part-two

--- src/js/main.js
+++ src/js/main.js
@@ -1,12 +1,13 @@
 'use strict';
 
-
+/*
+const url = 'api.disneyapi.dev/character?pageSize=50';
 // Paso 3: Obtener el array de personajes desde el API con fetch()
-fetch('api.disneyapi.dev/character?pageSize=50')
+fetch(url)
   .then(response => response.json())
   .then(data => {
   const characterData = data.results;
-  console.log(characterData);
+  console.log(characterData); //respuesta del servidor
   })
   .catch(error => {
     console.error('Error', error);
@@ -16,14 +17,17 @@ fetch('api.disneyapi.dev/character?pageSize=50')
 const characterData = [
   { name: "Mickey Mouse", image: "mickey.jpg" },
   { name: "Donald Duck", image: "donald.jpg" },
+  { name: "Mickey Mouse", image: "mickey.jpg" },
+  { name: "Donald Duck", image: "donald.jpg" },
+  { name: "Mickey Mouse", image: "mickey.jpg" },
+  { name: "Donald Duck", image: "donald.jpg" },
   // ... más personajes
 ];
 
-//1Generar HTML para un único personaje con bucle
+//1.Generar HTML para un único personaje con bucle
 const charactersContainer = document.querySelector('.js_character-list');
 for (let i = 0; i < characterData.length; i++) {
   const character = characterData[i];
-  
   const characterCard = `
     <div class="card">
       <p>${character.name}</p>
@@ -35,3 +39,40 @@ for (let i = 0; i < characterData.length; i++) {
   charactersContainer.innerHTML += characterCard;
 
 };
+
+*/
+let listCharactersApi = [];
+const ulElement = document.querySelector('.js_ul_list');
+const url = 'https://api.disneyapi.dev/character?pageSize=50';
+// Paso 3: Obtener el array de personajes desde el API con fetch()
+fetch(url)
+  .then(response => response.json())
+  .then(data => {
+  console.log(data); //respuesta del servidor
+  listCharactersApi = data.characters;
+  renderCharacterList (listCharactersApi);
+  ulElement.innerHTML =  renderCharacter(data.characters[0]);
+  })
+  .catch(error => {
+    console.error('Error', error);
+});
+
+function renderCharacterList (listData) {
+  for (const character of listData) {
+    ulElement.innerHTML += renderCharacter(character);
+  }
+}
+function renderCharacter(characters) {
+  let html = '';
+
+  for (const character of characters) { 
+    html += `
+      <div class="characters js_character-list">
+        "name": ${character.name},
+        "imageUrl": "${character.imageUrl}"
+      </div>
+    `;
+  }
+
+  return html;
+}
