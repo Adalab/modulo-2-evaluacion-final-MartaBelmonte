653abebf412305ce9ca3f00a52f09156bb62c3d4
MartaBelmonte <martabelmonts5@gmail.com>
2023-06-13 00:49:43 +0200

    proves

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -8,23 +8,23 @@ const ulFavorites = document.querySelector('.js_ul_favlist');
 const searchBox = document.querySelector(".js-search-box");
 const searchBtn = document.querySelector(".js-search-button");
 
-
+//Solicitud al servidor 
 fetch(url)
-  .then(response => response.json())
-  .then(data => {
-    listCharactersApi = data.data;
-    renderCharacterList(listCharactersApi);
+  .then(response => response.json()) //coge respuesta en formato JSON
+  .then(data => {  //para manejar la respuesta anterior
+    listCharactersApi = data.data;  //respuesta del servidor+propiedad "data"
+    renderCharacterList(listCharactersApi); //se mostrará la lista de characters
   })
   .catch(error => {
     console.error('Error', error);
 });
 
-
+//Renderizar characters
 function renderCharacterList(listData) {
   ulElement.innerHTML = ''; // Limpiar antes de renderizar
 
-  for (const character of listData) {
-    ulElement.innerHTML += renderCharacter(character); // Renderiza cada personaje y lo agrega a ulElement
+  for (const character of listData) { //bucle-iterar
+    ulElement.innerHTML += renderCharacter(character); //De agrega el HTML generado para el personaje al contenido HTML existente en ulElement.
   }
 
   addEventCharacter(); // Agrega el evento de click
@@ -62,8 +62,8 @@ function removeFavoriteCharacter(event) {
 
 
 function renderCharacter(character) { 
-  const isFavorite = listCharacterFavorite.some((item) => item._id === character._id);
-  const favoriteClass = isFavorite ? 'favorite' : '';
+  const isFavorite = listCharacterFavorite.some((item) => item._id === character._id); //id del item = id del character, se cumple la condicion (true)
+  const favoriteClass = isFavorite ? 'favorite' : '';  //condición ? expresion1(true)class'favorite' : expresion2(false)
   const html = `
     <div class="characters js_character-list ${favoriteClass}" id="${character._id}">
       <p>Name: ${character.name}</p>
@@ -73,8 +73,9 @@ function renderCharacter(character) {
   return html;
 }
 
-function renderFavoriteCharacter(character) { 
-   if (!character) {
+//Pintar al HTML el fav character
+function renderFavoriteCharacter(character) {    
+   if (!character) {                   
     return ''; 
   }
   const html = `
@@ -88,18 +89,19 @@ function renderFavoriteCharacter(character) {
   return html;
 }
 
+//Funcion del evento cuando se hace click en un character
 function handleClick(event) {
-  const id = event.currentTarget.id;
-  const selectedCharacter = listCharactersApi.find((item) => item._id === parseInt(id));
-  const indexCharacter = listCharacterFavorite.findIndex((item) => item._id === parseInt(id));
+  const id = event.currentTarget.id;  //se obtiene el valor del atributo 'id' y se guarda en la const
+  const selectedCharacter = listCharactersApi.find((item) => item._id === parseInt(id));  
+  const indexCharacter = listCharacterFavorite.findIndex((item) => item._id === parseInt(id)); //cuando un elemento no se encuentra en el array devuelve -1
   
-  if (indexCharacter === -1) {
-    listCharacterFavorite.push(selectedCharacter);
-    event.currentTarget.classList.add('favorite');
+  if (indexCharacter === -1) { //si da -1 : no está en favoritos
+    listCharacterFavorite.push(selectedCharacter);  //agrega el personaje seleccionado a la lista de favs
+    event.currentTarget.classList.add('favorite');  //agrega la clase "favorite" al elemento HTML en q se hizo click
     moveCharacterToFavorites(event.currentTarget); // Mover el personaje al contenedor de favoritos
   } else {
-    listCharacterFavorite.splice(indexCharacter, 1);
-    event.currentTarget.classList.remove('favorite');
+    listCharacterFavorite.splice(indexCharacter, 1); //se elimina el personaje de la lista fav
+    event.currentTarget.classList.remove('favorite');  //se elimina la clase "favorite"
     removeCharacterFromFavorites(id); // Eliminar el personaje del contenedor de favoritos
   }
 
@@ -121,9 +123,10 @@ function moveCharacterToFavorites(characterElement) {
   favoritesSection.classList.remove('hidden-section');
 }
 
+// Si la sección de favoritos está vacía: ocultarla 
 function removeCharacterFromFavorites(id) {
-    // Si la sección de favoritos está vacía: ocultarla 
   const favoriteCharacters = favoritesSection.querySelectorAll('.js_favorite-character');
+  
   if (favoriteCharacters.length === 0) {
     favoritesSection.classList.add('hidden-section');
   }
@@ -131,24 +134,22 @@ function removeCharacterFromFavorites(id) {
 
 //Add to LocalStorage
 function addRemoveIdIntoLocalStorage(id) {
-  const ids = JSON.parse(localStorage.getItem('characterIds')) || [];
+  const ids = JSON.parse(localStorage.getItem('characterIds')) || [];  //get:recupera el valor almacenado como string y el json.parse lo convierte en un array. Si el valor no es valido se da una const vacía. 
 
-  if (ids.includes(id)) {
-    // Si el ID está incluido, se elimina
-    const newIds = ids.filter((item) => item !== id);
-    localStorage.setItem('characterIds', JSON.stringify(newIds));
-  } else {
-    // Si el ID no está incluido, se agrega
-    ids.push(id);
-    localStorage.setItem('characterIds', JSON.stringify(ids));
+  if (ids.includes(id)) { // Si el ID está incluido en el LS: 
+    const newIds = ids.filter((item) => item !== id);  //
+    localStorage.setItem('characterIds', JSON.stringify(newIds));  //guarda la nueva const newIds en el LS convertido en una cadena de texto mediante json.stringify
+
+  } else {  // Si el ID no está incluido:
+    ids.push(id);  //se agrega al LS
+    localStorage.setItem('characterIds', JSON.stringify(ids)); //se guarda en el LS convertido en una cadena de texto mediante json.stringify
   }
 }
 
-
-//filter for searching a character
+//Filter to search
 function handleClickbtn(event) {
   event.preventDefault();
-  const searchValue = searchBox.value.toLowerCase();
+  const searchValue = searchBox.value.toLowerCase(); //para obtener el valor del texto
   const characterFilter = listCharactersApi.filter((character) =>
     character.name.toLowerCase().includes(searchValue)
   );
